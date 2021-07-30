314948d8a06b6c1cd059d58140919a66469b7287
Douglas Strey <douglas.morini@hotmail.com>
2021-07-30 19:40:29 -0300

    Iniciado exercícios

---
+++ aula-pratica05b/ContaBanco.php
@@ -0,0 +1,114 @@
+<?php
+
+class ContaBanco
+{
+    // Atributos
+    public $numContas;
+    protected $tipo;
+    private $dono;
+    private $saldo;
+    private $status;
+
+    // Métodos
+    public function abrirConta($tipo)
+    {
+        $this->setTipo($tipo);
+        $this->setStatus(true);
+
+        if ($tipo == "CC") {
+            $this->setSaldo(50);
+        } elseif ($tipo == "CP") {
+            $this->setSaldo(150);
+        }
+    }
+
+    public function fecharConta()
+    {
+        if ($this->getSaldo() > 0) {
+            echo "<p>ERRO! Conta com dinheiro, o senhor não pode fechar ainda. Primeiro saque seu dinheiro</p>";
+        } elseif ($this->getSaldo() < 0) {
+            echo "<p>ERRO! Conta em débito com o banco... Impossível encerrar!</p>";
+        } else {
+            $this->setStatus(false);
+        }
+    }
+
+    public function depositar($valor)
+    {
+        if ($this->getStatus() == true) {
+            $this->setSaldo($this->getSaldo() + $valor);
+        } else {
+            echo "<p>ERRO! Impossível depositar, esta conta não está ativa!</p>";
+        }
+    }
+
+    public function sacar($valor)
+    {
+        if ($this->getStatus() == true) {
+            if ($this->getSaldo() > $valor) {
+                $this->setSaldo($this->getSaldo() - $valor);
+            } else {
+                echo "<p>ERRO! Saldo insuficiente, Impossível sacar!";
+            }
+        } else {
+            echo "<p>ERRO! Impossível sacar, esta conta não está ativa!</p>";
+        }
+    }
+
+    public function pagarMensalidade()
+    {
+    }
+
+    // Métodos Especiais
+
+    public function ContaBanco()
+    {
+        $this->setSaldo(0);
+        $this->setStatus(false);
+    }
+
+    public function getNumConta()
+    {
+        return $this->numConta;
+    }
+    public function setNumConta($numConta)
+    {
+        $this->tipo = $numConta;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    public function getDono()
+    {
+        return $this->dono;
+    }
+    public function setDono($dono)
+    {
+        $this->dono = $dono;
+    }
+
+    public function getSaldo()
+    {
+        return $this->saldo;
+    }
+    public function setSaldo($saldo)
+    {
+        $this->saldo = $saldo;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
