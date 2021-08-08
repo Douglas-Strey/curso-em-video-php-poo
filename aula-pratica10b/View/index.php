bfee0800c2f714e0f91d048dbab21a864d014153
Douglas Strey <douglas.morini@hotmail.com>
2021-08-07 23:48:48 -0300

    Iniciado alterações no index

--- aula-pratica10b/View/index.php
+++ aula-pratica10b/View/index.php
@@ -1,2 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require_once 'Models/Aluno.php';
+    require_once 'Models/Funcionarios.php';
+    require_once 'Models/Pessoa.php';
+    require_once 'Models/Professor.php';
+
+
+    ?>
+</body>
+
+</html>
