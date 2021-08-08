0ea1959d4f4a7febe8c5cc90d3acf5c599b6b70e
Douglas Strey <douglas.morini@hotmail.com>
2021-08-07 23:46:54 -0300

    Finalizado classe Funcionários

--- aula-pratica10b/Models/Funcionarios.php
+++ aula-pratica10b/Models/Funcionarios.php
@@ -6,4 +6,27 @@ class Funcionarios extends Pessoa
 {
     private $setor;
     private $trabalhando;
+
+    public function mudarTrabalho()
+    {
+        $this->trabalhando != $this->trabalhando;
+    }
+
+    public function getSetor()
+    {
+        return $this->setor;
+    }
+    public function setSetor($setor)
+    {
+        $this->setor = $setor;
+    }
+
+    public function getTrabalhando()
+    {
+        return $this->trabalhando;
+    }
+    public function setTrabalhando($trabalhando)
+    {
+        $this->trabalhando = $trabalhando;
+    }
 }
