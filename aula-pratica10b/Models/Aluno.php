86e11cfa822d7489fcdbb950450b75375a8960dd
Douglas Strey <douglas.morini@hotmail.com>
2021-08-07 22:38:11 -0300

    Finalizado a classe Aluno

--- aula-pratica10b/Models/Aluno.php
+++ aula-pratica10b/Models/Aluno.php
@@ -8,5 +8,24 @@ class Aluno extends Pessoa
 
     public function cancelarMatricula()
     {
+        echo "<p>A sua matrícula será cancelada!</p>";
+    }
+
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+    }
+
+    public function getCurso()
+    {
+        return $this->curso;
+    }
+    public function setCurso($curso)
+    {
+        $this->curso = $curso;
     }
 }
