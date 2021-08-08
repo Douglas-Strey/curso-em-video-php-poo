1ee37cd4de28726c9ce49aa8d7f58a007b966226
Douglas Strey <douglas.morini@hotmail.com>
2021-08-08 00:05:56 -0300

    Finalizado aula 10b - Exercícios de Herança

--- aula-pratica10b/View/index.php
+++ aula-pratica10b/View/index.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once 'Models/Aluno.php';
+require_once 'Models/Funcionarios.php';
+require_once 'Models/Professor.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+$p1 = new Pessoa();
+$p2 = new Aluno();
+$p3 = new Professor();
+$p4 = new Funcionarios();
 
-<body>
-    <?php
-    require_once 'Models/Aluno.php';
-    require_once 'Models/Funcionarios.php';
-    require_once 'Models/Pessoa.php';
-    require_once 'Models/Professor.php';
+$p1->setNome("Pedro");
+$p2->setNome("Marias");
+$p3->setNome("Lucas");
+$p4->setNome("Lexi");
 
-
-    ?>
-</body>
-
-</html>
+print_r($p1);
