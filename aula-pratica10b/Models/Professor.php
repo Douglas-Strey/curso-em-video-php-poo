d237b2288f6d1d73919fb9aa1f2e1d09cbc5f383
Douglas Strey <douglas.morini@hotmail.com>
2021-08-07 23:40:10 -0300

    Finalizado classe Professor

--- aula-pratica10b/Models/Professor.php
+++ aula-pratica10b/Models/Professor.php
@@ -3,5 +3,29 @@ require_once 'Models/Pessoa.php';
 
 class Professor extends Pessoa
 {
-    
+    private $especialidade;
+    private $salario;
+
+    public function ReceberAumento($aum)
+    {
+        $this->salario += $aum;
+    }
+
+    public function getEspecialidade()
+    {
+        return $this->especialidade;
+    }
+    public function setEspecialidade($espec)
+    {
+        $this->especialidade = $espec;
+    }
+
+    public function getSalario()
+    {
+        return $this->salario;
+    }
+    public function setSalario($sal)
+    {
+        $this->salario = $sal;
+    }
 }
