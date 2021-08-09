7a7c03f7b3430889059e6f92902cacf2a049b9ca
Douglas Strey <douglas.morini@hotmail.com>
2021-08-09 11:10:11 -0300

    Adicionado Getters e Setters

--- aula-pratica11b/Pessoa.php
+++ aula-pratica11b/Pessoa.php
@@ -10,4 +10,31 @@ abstract class Pessoa
     {
         $this->idade++;
     }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+    }
 }
