3eaf4fa61a24482720e513b15712ede6a3c4e49c
Douglas Strey <douglas.morini@hotmail.com>
2021-08-09 11:11:36 -0300

    Modificado index.php

--- aula-pratica11b/index.php
+++ aula-pratica11b/index.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 11b</title>
+</head>
+
+<body>
+    <?php
+    require_once 'Pessoa.php';
+    ?>
+</body>
+
+</html>
