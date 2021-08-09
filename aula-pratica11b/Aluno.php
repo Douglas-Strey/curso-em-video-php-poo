2ab030f101fd288a5020095df4e8755208212726
Douglas Strey <douglas.morini@hotmail.com>
2021-08-09 11:22:20 -0300

    Finalizado classe Aluno.php

---
+++ aula-pratica11b/Aluno.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Pessoa.php';
+
+class Aluno extends Pessoa
+{
+    private $matricula;
+    private $curso;
+
+    public function pagarMensalidade()
+    {
+        echo "<p>Mensalidade do aluno $this->getNome foi paga!</p>";
+    }
+
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+    }
+
+    public function getCurso()
+    {
+        return $this->curso;
+    }
+    public function setCurso($curso)
+    {
+        $this->curso = $curso;
+    }
+}
