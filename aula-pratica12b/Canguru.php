8e698ab13a23332e8e73e0f394581b18776c67a3
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:44:13 -0300

    Finalizada classe Canguru.php

--- aula-pratica12b/Canguru.php
+++ aula-pratica12b/Canguru.php
@@ -1,2 +1,23 @@
 <?php
 
+require_once 'Mamifero.php';
+
+class Canguru extends Mamifero
+{
+    public function usarBolsa()
+    {
+        echo "<p>Usando bolsa</p>";
+    }
+    public function locomover()
+    {
+        echo "<p>EU TO CORRENDOOO</p>";
+    }
+    public function alimentar()
+    {
+        echo "<p>Mamando.</p>";
+    }
+    public function emitirSom()
+    {
+        echo "<p>MIAUUUUUUU.</p>";
+    }
+}
