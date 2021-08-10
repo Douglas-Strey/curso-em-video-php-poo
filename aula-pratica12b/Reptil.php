7bbd52091906275f132d32a5fb2402ab00bf8dcd
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 09:54:42 -0300

    Implementado métodos da classe Reptil.php

--- aula-pratica12b/Reptil.php
+++ aula-pratica12b/Reptil.php
@@ -1,1 +1,27 @@
 <?php
+
+require_once 'Animal.php';
+
+class Reptile extends Animal
+{
+    private $corEscama;
+
+    public function locomover()
+    {
+    }
+    public function alimentar()
+    {
+    }
+    public function emitirSom()
+    {
+    }
+
+    public function getCorEscama()
+    {
+        return $this->corEscama;
+    }
+    public function setCorEscama($corEscama)
+    {
+        $this->corEscama = $corEscama;
+    }
+}
