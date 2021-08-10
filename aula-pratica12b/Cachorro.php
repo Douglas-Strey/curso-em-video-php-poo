8185ba8752ae9ce0e5cc047f2210c26da9d36dcf
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:44:39 -0300

    Finalizada classe Cachorro.php

--- aula-pratica12b/Cachorro.php
+++ aula-pratica12b/Cachorro.php
@@ -1,1 +1,27 @@
 <?php
+
+require_once 'Mamifero.php';
+
+class Cachorro extends Mamifero
+{
+    public function enterrarOsso()
+    {
+        echo "<p>Enterrando osso</p>";
+    }
+    public function abanarRabo()
+    {
+        echo "<p>Abanando rabo</p>";
+    }
+    public function locomover()
+    {
+        echo "<p>EU TO CORRENDOOO</p>";
+    }
+    public function alimentar()
+    {
+        echo "<p>Mamando.</p>";
+    }
+    public function emitirSom()
+    {
+        echo "<p>Au Au au</p>";
+    }
+}
