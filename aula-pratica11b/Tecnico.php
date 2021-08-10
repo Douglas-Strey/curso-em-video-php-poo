d9089cd9cad27db29d0f76204b2866dd84cabee5
Douglas Strey <douglas.morini@hotmail.com>
2021-08-09 21:54:39 -0300

    Finalizada a classe Tecnico.php

---
+++ aula-pratica11b/Tecnico.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'Aluno.php';
+
+class Tecnico extends Aluno
+{
+    private $registroProfissional;
+
+    public function praticar()
+    {
+    }
+
+    public function getRegistroProfissional()
+    {
+        return $this->registroProfissional;
+    }
+    public function setRegistroProfissional($rp)
+    {
+        $this->registroProfissional = $rp;
+    }
+}
