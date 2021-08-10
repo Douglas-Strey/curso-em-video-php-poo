5f4e37e23bf46f1185567394e4fdba324ceb9481
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:01:00 -0300

    Finalizada classe Peixe.php

--- aula-pratica12b/Peixe.php
+++ aula-pratica12b/Peixe.php
@@ -1,1 +1,34 @@
 <?php
+
+require_once 'Animal.php';
+
+class Peixe extends Animal
+{
+    private $corEscama;
+
+    public function locomover()
+    {
+        echo "<p>EU TO NADANDO RÁPIDOOOOO</p>";
+    }
+    public function alimentar()
+    {
+        echo "<p>Comendo alguinhasss</p>";
+    }
+    public function emitirSom()
+    {
+        echo "<p>Blup blup blup</p>";
+    }
+    public function soltarBolha()
+    {
+        echo "<p>Soltei uma bolha ihull</p>";
+    }
+
+    public function getCorEscama()
+    {
+        return $this->corEscama;
+    }
+    public function setCorEscama($corEscama)
+    {
+        $this->corEscama = $corEscama;
+    }
+}
