c5d4fd2ff9450394388d18fbc24e0d726aa96d1b
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 09:38:47 -0300

    Finalizada os métodos Getters e Setters da classe Mamifero.php

--- aula-pratica12b/Mamifero.php
+++ aula-pratica12b/Mamifero.php
@@ -1,1 +1,27 @@
 <?php
+
+require_once 'Animal.php';
+
+class Mamifero extends Animal
+{
+    private $corPelo;
+
+    public function locomover()
+    {
+    }
+    public function alimentar()
+    {
+    }
+    public function emitirSom()
+    {
+    }
+
+    public function getCorPelo()
+    {
+        return $this->corPelo;
+    }
+    public function setCorPelo($corPelo)
+    {
+        $this->corPelo = $corPelo;
+    }
+}
