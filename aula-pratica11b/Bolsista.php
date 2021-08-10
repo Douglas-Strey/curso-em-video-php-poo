76297a5fe29f3999cc6440e6ecf91a0d2b8aa909
Douglas Strey <douglas.morini@hotmail.com>
2021-08-09 21:32:07 -0300

    Finalizada a classe Bolsista.php

--- aula-pratica11b/Bolsista.php
+++ aula-pratica11b/Bolsista.php
@@ -1,2 +1,27 @@
 <?php
 
+require_once 'Aluno.php';
+
+class Bolsista extends Aluno
+{
+    private $bolsa;
+
+    public function renovarBolsa()
+    {
+        echo "<p>Parabéns, sua bolsa foi renovada com sucesso!</p>"; // Aqui estou sobrepondo a função de Aluni.php;
+    }
+
+    public function pagarMensalidade()
+    {
+        echo "$this->getNome é bolsista, sua mensalidade tem desconto!</p>";
+    }
+
+    public function getBolsa()
+    {
+        return $this->bolsa;
+    }
+    public function setBolsa($bolsa)
+    {
+        $this->bolsa = $bolsa;
+    }
+}
