18f2a2f0cbc3695a4c54b646b8649e25c9786b22
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:36:39 -0300

    Adicionado os requires_once no index

--- aula-pratica12b/index.php
+++ aula-pratica12b/index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 12b</title>
+</head>
+
+<body>
+    <?php
+    require_once 'Mamifero.php';
+    require_once 'Ave.php';
+    require_once 'Reptil.php';
+    require_once 'Peixe.php';
+    ?>
+</body>
+
+</html>
