ca66caa78df777eec8fb3aabd7eb181a6a46e120
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:49:02 -0300

    Finalizada classe Cobra.php

--- aula-pratica12b/Cobra.php
+++ aula-pratica12b/Cobra.php
@@ -1,1 +1,19 @@
 <?php
+
+require_once 'Reptil.php';
+
+class Cobra extends Reptil
+{
+    public function locomover()
+    {
+        echo "<p>Rastejando</p>";
+    }
+    public function alimentar()
+    {
+        echo "<p>Matando</p>";
+    }
+    public function emitirSom()
+    {
+        echo "<p>Psiiiii</p>";
+    }
+}
