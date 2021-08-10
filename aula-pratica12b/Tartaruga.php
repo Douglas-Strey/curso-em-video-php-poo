b361655c8a333dc3025cfcc836b70b2cfa9b484e
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 10:50:57 -0300

    Finalizada classe Tartaruga.php

--- aula-pratica12b/Tartaruga.php
+++ aula-pratica12b/Tartaruga.php
@@ -1,1 +1,19 @@
 <?php
+
+require_once 'Reptil.php';
+
+class Tartaruga extends Reptil
+{
+    public function locomover()
+    {
+        echo "<p>Rastejando</p>";
+    }
+    public function alimentar()
+    {
+        echo "<p>Matando</p>";
+    }
+    public function emitirSom()
+    {
+        echo "<p>Sei lá que som faz</p>";
+    }
+}
