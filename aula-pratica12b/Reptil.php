bfae2cc2cf98f063e2ebe754cb8493aa9a9826b2
Douglas Strey <douglas.morini@hotmail.com>
2021-08-10 09:57:10 -0300

    Finalizada funções da classe Reptil.php

--- aula-pratica12b/Reptil.php
+++ aula-pratica12b/Reptil.php
@@ -8,12 +8,15 @@ class Reptile extends Animal
 
     public function locomover()
     {
+        echo "<p>EU TO VOANDO ALTOOOO</p>";
     }
     public function alimentar()
     {
+        echo "<p>Eu to comendo muita verdura mermão.</p>";
     }
     public function emitirSom()
     {
+        echo "<p>NHIC NHIC</p>";
     }
 
     public function getCorEscama()
