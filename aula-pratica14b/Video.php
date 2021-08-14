ccd7cbdeb85e8ccd2740e585e6e93a39894dba00
Douglas Strey <douglas.morini@hotmail.com>
2021-08-14 17:41:39 -0300

    Adicionado as funções criadas na interface na Classe

--- aula-pratica14b/Video.php
+++ aula-pratica14b/Video.php
@@ -4,5 +4,15 @@ require_once 'AcoesVideo.php';
 
 class Video implements AcoesVideo
 {
-    
+    public function play()
+    {
+    }
+
+    public function pause()
+    {
+    }
+
+    public function like()
+    {
+    }
 }
