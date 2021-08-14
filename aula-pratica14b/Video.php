0e08bfd371ff17df9e9d3d9c2b70ed5a844e8413
Douglas Strey <douglas.morini@hotmail.com>
2021-08-14 17:53:05 -0300

    Adicionado os atributos da classe Video.php

--- aula-pratica14b/Video.php
+++ aula-pratica14b/Video.php
@@ -4,6 +4,12 @@ require_once 'AcoesVideo.php';
 
 class Video implements AcoesVideo
 {
+    private $titulo;
+    private $avaliacao;
+    private $views;
+    private $curtida;
+    private $reproduzindo;
+    
     public function play()
     {
     }
